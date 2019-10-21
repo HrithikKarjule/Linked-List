77630d1bc535e7fa14448834e873386614512829
HrithikKarjule <53424143+HrithikKarjule@users.noreply.github.com>
2019-10-21 23:45:54 +0530

    Update linked list.c

--- linked list.c
+++ linked list.c
@@ -3,108 +3,29 @@
 
 struct node
 {
-int info;
-struct node*next;
-}*start=NULL;
-
-int add_end(int d)
-{
-	struct node*newcode,*temp;
-	newcode=(struct node*)malloc(sizeof(struct node));
-	newcode->info=d;
-	newcode->next=NULL;
-	temp=start;
-	while(temp->next!=NULL)
-	{
-		temp=temp->next;
-	}
-	temp->next=newcode;
-}
-
-int add_beg(int a)
-{
-struct node*newcode;
-	newcode=(struct node*)malloc(sizeof(struct node));
-	newcode->info=a;
-	newcode->next=start;
-	start=newcode;
-}
-
-int del_beg()
-{
-	struct node*newcode,*temp;
-	temp=start;
-	start=start->next;
-	free(temp);
-}
-
-int del_end()
-{
-	struct node*temp,*ptr;
-	temp=start;
-	while(temp->next->next!=NULL)
-	{
-		temp=temp->next;
-	}
-	ptr=temp->next;
-	temp->next=NULL;
-	free(ptr);
-}
-
-void display()
-{
-	struct node*temp;
-	temp=start;
-	printf("{");
-	while(temp!=NULL)
-	{
-		printf("%d,",temp->info);
-		temp=temp->next;
-	}
-	printf("}\n");
-}
+	int info;
+	struct node*next;
+};
 
 void main()
 {
-    int n;
-
-
-	printf("Choose any  function inorder to proceed further \n");
-	while(1)
-	{
-	printf("1. To add a number in the beginning \n");
-	printf("2. To add a number in the end \n");
-	printf("3. To delete the first number \n");
-	printf("4. To delete the last number \n");
-	printf("5. Display \n");
-	scanf("%d",&n);
-
-	switch(n)
-	{
-		case 1: add_beg(20);
-		        printf("Successfully Inserted");
-
-		break;
-
-		case 2: add_end(50);
-		        printf("Successfully Inserted");
-		break;
-
-		case 3: del_beg();
-		        printf("Successfully Deleted");
-		break;
-
-		case 4: del_end();
-		        printf("Successfully Deleted");
-		break;
-
-		case 5: printf("\n Nodes are: ");
-		        display();
-		        printf("\n");
-		break;
-
-		default : printf("Invalid Choice ");
-	}
-
-}
+	struct node*temp1,*temp2,*temp3;
+	temp1=(struct node*)malloc(sizeof(struct node));
+	temp2=(struct node*)malloc(sizeof(struct node));
+	temp3=(struct node*)malloc(sizeof(struct node));
+	
+	
+	temp1->info=2;
+	temp1->next=temp2;
+	printf("\n Data = %d  Present Address = %u  Next Address = %u ",temp1->info,temp1,temp1->next);
+	
+	temp2->info=3;
+	temp2->next=temp3;
+	
+	printf("\n Data = %d  Present Address = %u  Next Address = %u ",temp2->info,temp2,temp2->next);
+	
+	temp3->info=4;
+	temp3->next=NULL;
+	
+	printf("\n Data = %d  Present Address = %u  Next Address = %u ",temp3->info,temp3,temp3->next);
 }
